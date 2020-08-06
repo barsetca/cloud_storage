46c8e97ed3a0c1ea44cfa86b46bc7f5b0c8c36c1
Viktor Cherniak <barsetca@gmail.com>
2020-08-06 15:24:12 +0300

    cloud netty v.01

---
+++ cloud_common/src/main/java/FileRequestMessage.java
@@ -0,0 +1,12 @@
+public class FileRequestMessage extends AbstractMessage{
+
+    private String fileName;
+
+    public FileRequestMessage(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+}
---
+++ cloud_server/src/main/java/MainHandler.java
@@ -0,0 +1,30 @@
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class MainHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (msg instanceof FileRequestMessage) {
+            FileRequestMessage frm = (FileRequestMessage) msg;
+            if (Files.exists(Paths.get("serverFiles/" + frm.getFileName()))) {
+                FileSendMessage fsm = new FileSendMessage(Paths.get("serverFiles/" + frm.getFileName()));
+                ctx.writeAndFlush(fsm);
+            }
+        }
+        if (msg instanceof FileSendMessage) {
+            FileSendMessage fsm = (FileSendMessage) msg;
+            Files.write(Paths.get("serverFiles/" + fsm.getFileName()), fsm.getContent(), StandardOpenOption.CREATE);
+        }
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
